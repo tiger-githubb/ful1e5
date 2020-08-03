8717e2012f88ae0d27d0982403d7c04be25525e8
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-08-03 09:38:30 +0530

    ✨ Ninja Badge

--- api/badge.ts
+++ api/badge.ts
@@ -1,21 +1,11 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+import { NinjaBadge } from "../components/NinjaBadge";
 import { renderToString } from "react-dom/server";
-import { decode } from "querystring";
-import { Image } from "../components/Image";
 
 export default async function (req: NowRequest, res: NowResponse) {
-  const params = decode(req.url.split("?")[1]) as any;
-
   res.setHeader("Content-Type", "image/svg+xml");
   res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
 
-  const text = renderToString(
-    Image({
-      width: 160,
-      height: 28,
-      src: "../assets/ninja_badge.svg",
-      imageId: "Made By Kaiz",
-    })
-  );
-  return res.status(200).send(text);
+  const badge = renderToString(NinjaBadge({}));
+  return res.status(200).send(badge);
 }
